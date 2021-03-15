5c63d1bd91b9ed96fa17658d367bfae59ddf11f1
Mac Me <macme.tang@gmail.com>
2021-03-16 05:25:50 +0530

    stackupdate

--- test/cdkpipelines-skel.test.ts
+++ test/cdkpipelines-skel.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CdkpipelinesDemo from '../lib/stack-containers/lambda-stack/lambda';
+import * as LambdaTestStack from '../lib/stack-containers/lambda-stack/lambda';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkpipelinesDemo.LambdaStack(app, 'MyTestStack');
+    const stack = new LambdaTestStack.LambdaStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
