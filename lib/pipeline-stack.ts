04a4bab5f192e8374ef1ccc9189a40a442c5dadc
Mac Me <macme.tang@gmail.com>
2021-04-03 08:56:00 +0530

    update

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -96,7 +96,7 @@ export class CdkPipelineStack extends Stack {
             'cd ansible-cb',
             'pwd && ls',
             'printenv',
-            'aws ec2 describe-instances --region us-east-1'
+            //'aws ec2 describe-instances --region us-east-1'
             //'ansible-playbook win_ping.yml'
 
             ]
